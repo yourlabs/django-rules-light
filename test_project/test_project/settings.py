33ede0798c50d8a58a9aff3e651a7044be49fe32
jpic <jamespic@gmail.com>
2012-11-25 20:09:18 +0100

    Enabled rules_light logging in test_project

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -156,13 +156,22 @@ LOGGING = {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
-        }
+        },
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+        },
     },
     'loggers': {
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
             'propagate': True,
+        },
+        'rules_light': {
+            'handlers':['console'],
+            'propagate': True,
+            'level':'DEBUG',
         },
     }
 }
