26bc8be014f85c7f5bb6a6afeaa85b58108d2c7d
jpic <jamespic@gmail.com>
2013-01-29 11:14:20 +0100

    Version bump 0.0.6

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class RunTests(Command):
 
 setup(
     name='django-rules-light',
-    version='0.0.5',
+    version='0.0.6',
     description='Rule registry for django',
     author='James Pic',
     author_email='jamespic@gmail.com',
