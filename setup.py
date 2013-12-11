7fcac4a73b477c56e1e54afb2bc08c8743c85899
Fábio C. Barrioneuvo da Luz <bnafta@gmail.com>
2013-12-11 13:58:32 -0300

    Increase version, added autor

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ else:
 
 setup(
     name='django-rules-light',
-    version='0.1.1',
+    version='0.1.2',
     description='Rule registry for django',
     author='James Pic',
     author_email='jamespic@gmail.com',
