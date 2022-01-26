7a603b6368ea507af8c896dbf83ba9949bda4994
Serafeim Papastefanos <spapas@gmail.com>
2022-01-26 11:10:21 +0200

    Update the urls

--- rules_light/urls.py
+++ rules_light/urls.py
@@ -1,10 +1,10 @@
 from __future__ import unicode_literals
 
-from django.conf.urls import url
+from django.urls import re_path
 
 from .views import RegistryView
 
 
 urlpatterns = [
-    url(r'$', RegistryView.as_view(), name='rules_light_registry'),
+    re_path(r'$', RegistryView.as_view(), name='rules_light_registry'),
 ]
